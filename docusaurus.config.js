96bac3ca5f8805175b662f71afb0f6ff0551c84b
Hideto Yasunori <hideto0118yasunori@gmail.com>
2022-03-26 09:28:37 -0700

    Eddit site info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'hideto0118.github.io',
+  baseUrl: '/docusaurus-test',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'hideto.org', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-test', // Usually your repo name.
   // themes: ['@docusaurus/theme-search-algolia'],
 
   presets: [
