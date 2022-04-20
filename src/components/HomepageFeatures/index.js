2ef6bb8ff00173faf777b9f2d71203e7c4c75363
Hideto Yasunori <hideto0118yasunori@gmail.com>
2022-04-20 12:05:53 -0700

    Add Top page link - still wip

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,50 +1,53 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'White Paper',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    linkTo: "/docs/whitepaper/intro",
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Oasys is an EVM-Compatible protocol that adopts the Oasys Architecture. This unique architecture consists of a multi-layered structure: Hub-Layer, a highly scalable Layer 1; and Verse-Layer, a special Layer 2 using Ethereum's Layer 2 scaling solution.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Technical Materials',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    linkTo: "/docs/techdocs/intro",
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Guides on how to build nodes and run networks.
       </>
     ),
   },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  // {
+  //   title: 'Powered by React',
+  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       Extend or customize your website layout by reusing React. Docusaurus can
+  //       be extended while reusing the same header and footer.
+  //     </>
+  //   ),
+  // },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Svg, linkTo, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
+    <div className={clsx('col col--6')}>
+      <Link className={styles.topPageLink} to={linkTo}>
+        <div className="text--center">
+          {/* <Svg className={styles.featureSvg} role="img" /> */}
+        </div>
+        <div className="text--center padding-horiz--lg">
+          <h2 className={styles.largeText}>{title}</h2>
+          <p className={styles.smallText} >{description}</p>
+        </div>
+      </Link>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,11 +14,11 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
+          {/* <Link
             className="button button--secondary button--lg"
             to="/docs/whitepaper/intro">
             Read Oasys White Paper
-          </Link>
+          </Link> */}
         </div>
       </div>
     </header>
