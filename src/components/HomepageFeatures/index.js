167d4bd3e9b7bd67ac5f5eae16d4fdafff8f2168
Hideto Yasunori <hideto0118yasunori@gmail.com>
2022-04-20 15:10:50 -0700

    Modify top page with shadow and dark mode link bg

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -10,7 +10,7 @@ const FeatureList = [
     linkTo: "/docs/whitepaper/intro",
     description: (
       <>
-        Oasys is an EVM-Compatible protocol that adopts the Oasys Architecture. This unique architecture consists of a multi-layered structure: Hub-Layer, a highly scalable Layer 1; and Verse-Layer, a special Layer 2 using Ethereum's Layer 2 scaling solution.
+        Oasys is an EVM-Compatible protocol that adopts the Oasys Architecture. 
       </>
     ),
   },
