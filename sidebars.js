6ea6594d18b3ff018c829ad68f40c57faaeb12d3
fromreto <107421475+fromreto@users.noreply.github.com>
2022-11-08 19:03:23 +0900

    Update ether.js / update sidebar.js
    Update ether.js / update sidebar.js

--- sidebars.js
+++ sidebars.js
@@ -69,7 +69,6 @@ const sidebars = {
       },
       items: [
           'techdocs/validator/hardware-requirements/1-1',
-          'techdocs/validator/chain-environment/1-1',
           'techdocs/validator/hub-layer-node-build/1-1',
           'techdocs/validator/hub-layer-client-join/1-1',
           'techdocs/validator/hub-layer-client-join/1-2',
@@ -79,6 +78,16 @@ const sidebars = {
           'techdocs/validator/verse-layer-node-build/1-1',
           'techdocs/validator/verse-proxy-build/1-1',
           'techdocs/validator/faq/1-1',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Chain Environment',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+          'techdocs/validator/chain-environment/1-1',
       ],
     },
     {
